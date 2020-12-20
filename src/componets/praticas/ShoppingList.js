ba5bb709e38a8e7cbd18c93e9661cb7ed8ce66bd
Andrey Nithack <andreynithack@gmail.com>
2020-12-20 00:58:33 -0300

    Finishi code tutorial

---
+++ src/componets/praticas/ShoppingList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ShoppingList extends React.Component {
+    render(){(
+        <div className="shopping-list">
+            <ul>
+                <li>Instagram</li>
+                <li>WhatsApp</li>
+                <li>Linkedin</li>
+            </ul>
+        </div>
+    );
+ }
+}
